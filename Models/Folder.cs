8a819786a32d4268adf3132d95a62f944cefb443
IsaGuzman55 <isabellaguzman143@gmail.com>
2024-06-26 16:13:16 -0500

    Creación de modelos con sus validaciones

---
+++ Models/Folder.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Backend.Models;
+
+namespace Backend.Models{
+    public class Folder{
+
+        /* --------------- */
+        public int Id {get; set;}
+
+        /* ---------------------- */
+        public int UserId {get; set;}
+        public User? User {get; set;}
+
+        /* --------------- */
+        public int ParentFolderId{get; set;}
+
+        /* ---------------- */
+        [Required(ErrorMessage = "The Name of the folder is required.")]
+        [MinLength(1, ErrorMessage = "Name must be at least {1} characters.")]
+        [MaxLength(50, ErrorMessage = "Name must be at most {1} characters.")]
+        public string Name {get; set;}
+
+        /* -------------- */
+        [Required(ErrorMessage = "The Data of the folder is required.")]
+        [DataType(DataType.Text)]
+        public string Data {get; set;}
+
+        /* --------------- */
+        [Required(ErrorMessage = "The date of creation is required.")]
+        [DataType(DataType.Date)]
+        public DateOnly CreationDate {get; set;}
+
+        /* ------------- */
+        [Required(ErrorMessage = "status is required.")]
+        public string Status {get; set;}
+
+        /* ---------------- */
+        [JsonIgnore]
+        public List<Trash>? Trash{ get; set; }
+
+    }
+}
